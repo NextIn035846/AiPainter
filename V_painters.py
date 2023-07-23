2327c76cd4e53a2feb315274bf54e263f1f1f20e
Thomas Markas Patole <thomaspatole19@gmail.com>
2023-07-23 09:39:23 +0530

    Add files via upload

---
+++ V_painters.py
@@ -0,0 +1,119 @@
+import cv2
+import numpy as np
+import time
+import os
+import HandTrakingModule as htm
+
+######################
+brushThickness = 15
+eraserThickness= 25
+
+###################
+
+
+
+folderpath = 'Header'
+#get the all the image names
+myList = os.listdir(folderpath)
+drawColor = (255,255,0)
+
+overlay = []
+
+for impath in myList:
+    image= cv2.imread(f'{folderpath}/{impath}')
+    overlay.append(image)
+
+header = overlay[0]
+cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+
+detector = htm.handDetector(detectionCon=0.85)
+
+xp,yp = 0,0
+
+imgCanvas = np.zeros((720,1280,3),np.uint8)
+
+while True:
+    # import the images
+    success,img = cap.read()
+    img = cv2.flip(img,1)
+
+    # find the hand Landmarks
+    img= detector.findHands(img)
+    lmList,bbox= detector.findPosition(img,draw=False)
+
+    if len(lmList) != 0:
+
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        fingers= detector.fingersUp()
+
+        #If Selection Mode- Two finger are up
+        if fingers[1] and fingers[2]:
+            xp, yp = 0, 0
+
+            #print("Selcetion Mode")
+            #cheking for the click
+
+            if y1<125:
+                if 250<x1<450:
+                    header = overlay[0]
+                    drawColor=(255,0,0)
+                elif 550<x1< 650:
+                    header= overlay[1]
+                    drawColor=(0,0,255)
+                elif 750< x1<950:
+                    header = overlay[2]
+                    drawColor=(0,255,0)
+                elif 1050<x1<1200:
+                    header = overlay[3]
+                    drawColor=(0,0,0)
+
+            cv2.rectangle(img, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv2.FILLED)
+
+        # If drawing mode - index finger is up
+        if fingers[1] and fingers[2]== False:
+            cv2.circle(img,(x1,y1),20,drawColor,cv2.FILLED)
+            #print ("Drawing Mode")
+
+            if xp==0 and yp==0:
+                xp,yp = x1,y1
+
+            if drawColor ==(0,0,0):
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, eraserThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
+
+            else:
+                cv2.line(img,(xp,yp),(x1,y1),drawColor,brushThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+            xp,yp = x1,y1
+
+    imgGray = cv2.cvtColor(imgCanvas,cv2.COLOR_BGR2GRAY)
+    _, imgInv = cv2.threshold(imgGray,50,255,cv2.THRESH_BINARY_INV)
+    imgInv = cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)
+    img = cv2.bitwise_and(img,imgInv)
+    img = cv2.bitwise_or(img,imgCanvas)
+
+
+
+
+
+
+
+
+    #seting the header image
+    # our image is metrics so we know that where is our image is lies
+    #silcining the image swe know that our image size
+    img[0:125,0:1280]= header
+    img = cv2.addWeighted(img,0.5,imgCanvas,0.5,0)
+    cv2.imshow("Image",img)
+    cv2.imshow("Canvas",imgCanvas)
+    cv2.waitKey(1)
+
+
+
+
+
+
